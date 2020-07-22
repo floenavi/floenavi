176f87306ff5e1ccd1505e1bf882e4942d4574ab
Ingo Brunkhorst <Ingo.Brunkhorst@zuehlke.com>
2020-07-22 10:57:17 +0200

    Added pdfs

--- bin/update-docs.sh
+++ bin/update-docs.sh
@@ -4,3 +4,8 @@ pandoc -s -o README.html README.md
 pandoc -s -o UPGRADE.html UPGRADE.md
 pandoc -s -o FEATURES.html FEATURES.md
 pandoc -s -o RELEASE-NOTES.html RELEASE-NOTES.md
+
+pandoc -s -o README.pdf README.md
+pandoc -s -o UPGRADE.pdf UPGRADE.md
+pandoc -s -o FEATURES.pdf FEATURES.md
+pandoc -s -o RELEASE-NOTES.pdf RELEASE-NOTES.md
